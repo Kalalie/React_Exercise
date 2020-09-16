2a64b2cb8d99918b8701d797e9478a9f46a9e6e6
Kalalie <67173676+Kalalie@users.noreply.github.com>
2020-09-16 20:08:08 +0800

    Exercise1 completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // variables
+  const [count, setCount] = useState(0);
+  // methods
+
+  const add = () =>{ setCount(count +1);
+  };
+  const minus = () =>{ setCount(count -1);
+  };
+
+  // template
+  return(
+    <div class="App">
+      <h1> Counter</h1>
+      <button onClick={add} > Increase </button>
+      <span> {count} </span>
+      <button onClick={minus}> Decrease </button>
+
     </div>
   );
 }
-
 export default App;
